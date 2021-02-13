db25fd1b29431cc544b02763de591ea03886bba2
Themba Mathebula <tpmathebula@gmail.com>
2021-02-13 10:26:43 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
